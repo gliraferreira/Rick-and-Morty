b2140116307636419ad7c7d18604bbb44b13963c
Gabriel Lira <liraferreiragabriel@gmail.com>
2023-09-05 15:56:56 -0300

    Fixing navigation

--- app/src/main/java/br/com/lira/rickandmorty/main/MainActivity.kt
+++ app/src/main/java/br/com/lira/rickandmorty/main/MainActivity.kt
@@ -3,12 +3,17 @@ package br.com.lira.rickandmorty.main
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import br.com.lira.rickandmorty.R
 import br.com.lira.rickandmorty.core.toolkit.setupFragmentHost
 import br.com.lira.rickandmorty.databinding.ActivityMainBinding
 import br.com.lira.rickandmorty.features.characterslist.presentation.view.CharactersFragment
 import br.com.lira.rickandmorty.features.episodes.presentation.view.EpisodesFragment
 import br.com.lira.rickandmorty.features.locations.presentation.view.LocationsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -24,19 +29,10 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setupFragmentHost(R.id.app_nav_host_fragment, charactersFragment)
 
-        binding.navView.setOnItemSelectedListener {
+        val navView: BottomNavigationView = binding.navView
 
-            val fragment = when (it.itemId) {
-                R.id.navigation_characters -> charactersFragment
-                R.id.navigation_episodes -> episodesFragment
-                else -> locationsFragment
-            }
-
-            setupFragmentHost(R.id.app_nav_host_fragment, fragment)
-
-            true
-        }
+        val navController = findNavController(R.id.app_nav_host_fragment)
+        navView.setupWithNavController(navController)
     }
 }
