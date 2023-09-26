062823f9e3adbb96d21d992ccdd21ce63a8aad4d
Gabriel Lira <liraferreiragabriel@gmail.com>
2023-09-26 00:04:19 -0300

    Improving error mapper

--- app/src/main/java/br/com/lira/rickandmorty/features/characters/presentation/mapper/CharactersErrorMapper.kt
+++ app/src/main/java/br/com/lira/rickandmorty/features/characters/presentation/mapper/CharactersErrorMapper.kt
@@ -6,34 +6,31 @@ import br.com.lira.rickandmorty.main.presentation.model.GenericUIError
 import retrofit2.HttpException
 import javax.inject.Inject
 
+private const val HTTP_NOT_FOUND = 404
+
 class CharactersErrorMapper @Inject constructor(
     private val resourceProvider: ResourceProvider
 ) {
 
     fun mapFrom(error: Throwable): GenericUIError = when (error) {
-        is HttpException -> {
-            mapHttpException(error)
-        }
-        else -> {
-            GenericUIError(
-                message = resourceProvider.getString(R.string.common_error_title),
-                image = resourceProvider.getDrawable(R.drawable.ic_error),
-                isTryAgainVisible = true
-            )
-        }
+        is HttpException -> mapHttpException(error)
+        else -> mapGenericError()
     }
 
-    private fun mapHttpException(error: HttpException) = if (error.code() == 404) {
-        GenericUIError(
-            message = resourceProvider.getString(R.string.characters_search_empty_text),
-            image = resourceProvider.getDrawable(R.drawable.ic_empty),
-            isTryAgainVisible = false
-        )
-    } else {
-        GenericUIError(
-            message = resourceProvider.getString(R.string.common_error_title),
-            image = resourceProvider.getDrawable(R.drawable.ic_error),
-            isTryAgainVisible = true
-        )
+    private fun mapHttpException(error: HttpException) = when (error.code()) {
+        HTTP_NOT_FOUND -> mapNotFoundError()
+        else -> mapGenericError()
     }
+
+    private fun mapNotFoundError() = GenericUIError(
+        message = resourceProvider.getString(R.string.characters_search_empty_text),
+        image = resourceProvider.getDrawable(R.drawable.ic_empty),
+        isTryAgainVisible = false
+    )
+
+    private fun mapGenericError() = GenericUIError(
+        message = resourceProvider.getString(R.string.common_error_title),
+        image = resourceProvider.getDrawable(R.drawable.ic_error),
+        isTryAgainVisible = true
+    )
 }
